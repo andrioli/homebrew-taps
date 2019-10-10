bca4a5979c726653f0cea7ef739bd55b55d22c95
Roberto Araujo <roberto.andrioli@gmail.com>
2019-10-10 11:11:19 -0300

    Oracle JDK 7

---
+++ Casks/oracle-jdk7.rb
@@ -0,0 +1,41 @@
+cask 'oracle-jdk7' do
+  version '1.7.0_80'
+  url 'https://www.dropbox.com/s/3x4aagtg94xryln/jdk-7u80-macosx-x64.dmg?dl=1'
+  sha256 '2b9deef240a7f07d08541da01bbd60cbf93bf713efd5997e586ba23ec4f5089e'
+  name 'Oracle Java 7 Standard Edition Development Kit'
+
+  pkg 'JDK 7 Update 80.pkg'
+
+  postflight do
+    system '/usr/bin/sudo', '-E', '--',
+           '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string BundledApp', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+           '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string JNI',        "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+           '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string WebStart',   "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+           '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string Applets',    "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+           '/bin/mkdir', '-p', '--', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/bundle/Libraries"
+    system '/usr/bin/sudo', '-E', '--',
+           '/bin/ln', '-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/jre/lib/server/libjvm.dylib", "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
+    if MacOS.version <= :mavericks
+      system '/usr/bin/sudo', '-E', '--',
+             '/bin/rm', '-rf', '--', '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK'
+      system '/usr/bin/sudo', '-E', '--',
+             '/bin/ln', '-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents", '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK'
+    end
+  end
+
+  uninstall pkgutil: 'com.oracle.jdk7u80',
+            delete:  [
+                       MacOS.version <= :mavericks ? '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK' : '',
+                     ].keep_if { |v| !v.empty? }
+
+  zap       delete: [
+                      '~/Library/Application Support/Oracle/Java',
+                      '~/Library/Caches/com.oracle.java.Java-Updater',
+                      '~/Library/Caches/net.java.openjdk.cmd',
+                    ],
+            rmdir:  '~/Library/Application Support/Oracle/'
+end
